7635b6a01f01c99b1e60961b74ba3a94c0cb6089
Matt <matthewsimonmould@gmail.com>
2015-12-12 16:28:24 +0000

    Created layout

---
+++ app/src/main/java/uk/co/mould/matt/vocabinator/AndroidQueryView.java
@@ -0,0 +1,19 @@
+package uk.co.mould.matt.vocabinator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class AndroidQueryView extends LinearLayout {
+    public AndroidQueryView(Context context) {
+        super(context);
+    }
+
+    public AndroidQueryView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AndroidQueryView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
