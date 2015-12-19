5a2b121883cabea8f7b1b6e8bcab672013cc31a4
Matt <matthewsimonmould@gmail.com>
2015-12-19 16:29:55 +0000

    Refactoring test.

--- app/src/androidTest/java/uk/co/mould/matt/vocabinator/QueryViewTest.java
+++ app/src/androidTest/java/uk/co/mould/matt/vocabinator/QueryViewTest.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 public final class QueryViewTest extends AndroidTestCase {
 
     private AndroidQueryView queryView;
+    private CapturingQueryButtonListener queryButtonListener;
 
     @Override
     public void setUp() throws Exception {
@@ -15,18 +16,16 @@ public final class QueryViewTest extends AndroidTestCase {
         Context context = getContext();
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         queryView = (AndroidQueryView)layoutInflater.inflate(R.layout.query_layout, null);
+        queryButtonListener = new CapturingQueryButtonListener();
+        queryView.addQueryButtonListener(queryButtonListener);
     }
 
     public void testThatWhenButtonIsPressedListenersAreInvoked() {
-        CapturingQueryButtonListener queryButtonListener = new CapturingQueryButtonListener();
-        queryView.addQueryButtonListener(queryButtonListener);
         queryView.findViewById(R.id.query_button).performClick();
         assertTrue(queryButtonListener.invoked);
     }
 
     public void testThatCanGetTextFromTextBox() {
-        CapturingQueryButtonListener queryButtonListener = new CapturingQueryButtonListener();
-        queryView.addQueryButtonListener(queryButtonListener);
         String text = "some text";
         ((EditText)queryView.findViewById(R.id.query_entry_box)).setText(text);
         assertEquals(text, queryView.getTextBoxString());
