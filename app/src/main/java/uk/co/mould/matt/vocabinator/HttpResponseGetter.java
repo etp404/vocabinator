01ff00dc4b0583d99c09a20a5ffa918923af70da
Matt <matthewsimonmould@gmail.com>
2015-12-20 09:47:34 +0000

    First version of HttpGetter

--- app/src/main/java/uk/co/mould/matt/vocabinator/HttpResponseGetter.java
+++ app/src/main/java/uk/co/mould/matt/vocabinator/HttpResponseGetter.java
@@ -1,5 +1,7 @@
 package uk.co.mould.matt.vocabinator;
 
+import android.os.AsyncTask;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,28 +12,36 @@ import java.net.URL;
 
 class HttpResponseGetter implements ResponseGetter {
     @Override
-    public void getJsonForWord(ResponseGetterCallback responseGetterCallback) {
-        String result = "";
+    public void getJsonForWord(final ResponseGetterCallback responseGetterCallback) {
+        AsyncTask<Void, Void, Void> asyncTask = new AsyncTask<Void, Void, Void>(){
 
-        URL url = null;
-        try {
-            url = new URL("https://glosbe.com/gapi/translate?from=fra&dest=eng&format=json&phrase=ainsi");
-            String line;
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            while ((line = rd.readLine()) != null) {
-                result += line;
+            @Override
+            protected Void doInBackground(Void... params) {
+                String result = "";
+
+                URL url = null;
+                try {
+                    url = new URL("https://glosbe.com/gapi/translate?from=fra&dest=eng&format=json&phrase=ainsi");
+                    String line;
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = rd.readLine()) != null) {
+                        result += line;
+                    }
+                    rd.close();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                responseGetterCallback.success(result);
+                return null;
             }
-            rd.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        responseGetterCallback.success(result);
+        };
+        asyncTask.execute();
     }
 
 }
