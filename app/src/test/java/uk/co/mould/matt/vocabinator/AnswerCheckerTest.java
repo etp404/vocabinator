3f1df669bb57086a84f5dfebf6a33b9e51a27e21
Matt <matthewsimonmould@gmail.com>
2016-01-02 17:02:12 +0000

    Answer checker returns incorrect answer (possibly pointlessly).

--- app/src/test/java/uk/co/mould/matt/vocabinator/AnswerCheckerTest.java
+++ app/src/test/java/uk/co/mould/matt/vocabinator/AnswerCheckerTest.java
@@ -1,25 +1,42 @@
 package uk.co.mould.matt.vocabinator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 
 import uk.co.mould.matt.vocabinator.quiz.AnswerChecking;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AnswerCheckerTest {
 
+    private String rightAnswer = "hello";
+    private AnswerChecker answerChecker;
+    private CapturingCallback capturingCallback;
+
+    @Before
+    public void setUp() throws Exception {
+        answerChecker = new AnswerChecker();
+        capturingCallback = new CapturingCallback();
+    }
+
     @Test
     public void testThatAnswerCheckerReportsCorrectAnswerForCorrectAnswer() {
-        AnswerChecker answerChecker = new AnswerChecker();
-        CapturingCallback capturingCallback = new CapturingCallback();
-        answerChecker.check("hello", "hello", capturingCallback);
+        answerChecker.check(rightAnswer, rightAnswer, capturingCallback);
         assertTrue(capturingCallback.correctCalled);
+    }
+
+    @Test
+    public void testThatAnswerCheckerGivesCorrectIfReceivesIncorrectAnswer() {
+        answerChecker.check(rightAnswer, "wrong answer", capturingCallback);
+        assertEquals(capturingCallback.incorrectCalledWith, rightAnswer);
     }
 
     private static class CapturingCallback implements AnswerChecking.Callback {
         public boolean correctCalled = false;
+        public String incorrectCalledWith;
 
         @Override
         public void correct() {
@@ -27,16 +44,19 @@ public class AnswerCheckerTest {
         }
 
         @Override
-        public void incorrect(String corrrection) {
-
+        public void incorrect(String correction) {
+            incorrectCalledWith = correction;
         }
     }
 
     private class AnswerChecker implements AnswerChecking {
         @Override
-        public void check(String userAnswer, String correctAnswer, Callback callback) {
+        public void check(String correctAnswer, String userAnswer, Callback callback) {
             if (correctAnswer.equals(userAnswer)) {
                 callback.correct();
+            }
+            else {
+                callback.incorrect(correctAnswer);
             }
         }
     }
